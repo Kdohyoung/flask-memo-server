4c9287053b2a00389aa27382730330820c989190
Kdohyoung <asd62126310@gmail.com>
2022-06-21 17:44:29 +0900

    6-21

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
-from flask_jwt_extended import JWTManager
+from flask_jwt_extended import JWTManager 
 from flask_restful import Api
 from config import Config
-from recoources.user import UserRegisterResourse
+from resources.user import UserRegisterResourse 
+from resources.user import UseLoginResource , UserLogoutResource ,jwt_blacklist
+from resources.memo import MemoListResource
+from resources.memo_info import MemoResource
+from resources.memo_publish import MemoPublishResource
 
 
 app  = Flask(__name__)
@@ -10,12 +14,22 @@ app  = Flask(__name__)
 app.config.from_object(Config)
 
 jwt = JWTManager(app)
+
+# 로그아웃 된 토큰이 들어있는 set을, jwt 에 알려준다.
+@jwt.token_in_blocklist_loader
+def check_if_token_is_revoked(jwt_header, jwt_payload):
+    jti = jwt_payload['jti']
+    return jti in jwt_blacklist
+
 api  = Api(app)
 
 # 경로와 리소스를 연결 
 api.add_resource(UserRegisterResourse,'/users/register')
-
-
+api.add_resource(UseLoginResource,'/users/login')
+api.add_resource(UserLogoutResource,'/users/logout')
+api.add_resource(MemoResource,'/memos/<int:memo_id>')
+api.add_resource(MemoListResource,'/memos')
+api.add_resource(MemoPublishResource, '/memos/<int:memo_id>/publish')
 
 
 if __name__ == '__main__':
